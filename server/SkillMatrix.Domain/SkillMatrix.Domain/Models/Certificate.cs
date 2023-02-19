eabb4f8c872f68fe3f370bdd3ddcede2c1e92256
jometjoseph <120706019+jometjoseph@users.noreply.github.com>
2023-02-19 11:22:36 +0530

    Add files via upload

---
+++ server/SkillMatrix.Domain/SkillMatrix.Domain/Models/Certificate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+<<<<<<< HEAD
+=======
+using SkillMatrix.Domain.Types;
+>>>>>>> 8e5fdc9 (Server files)
+
+namespace SkillMatrix.Domain.Models
+{
+    public class Certificate
+    {
+        public int Id { get; set; }
+
+        [Required, NotNull]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+<<<<<<< HEAD
+        public enum Status { Approved = 1, pending = 2};
+=======
+        public Status Status { get; set; }
+>>>>>>> 8e5fdc9 (Server files)
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+    }
+}
---
+++ server/SkillMatrix.Domain/SkillMatrix.Domain/Models/UserSkill.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+<<<<<<< HEAD
+=======
+using SkillMatrix.Domain.Types;
+>>>>>>> 8e5fdc9 (Server files)
+
+namespace SkillMatrix.Domain.Models
+{
+    public class UserSkill
+    {
+        public int Id { get; set; }
+
+<<<<<<< HEAD
+        public StatusType.Type Type { get; set; }
+
+        public StatusType.Proficiency Proficiency { get; set; }
+=======
+        public SkillType SkillType { get; set; }
+
+        public Proficiency Proficiency { get; set; }
+>>>>>>> 8e5fdc9 (Server files)
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+<<<<<<< HEAD
+=======
+        public string ApplicationUserId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+>>>>>>> 8e5fdc9 (Server files)
+        public int SkillId { get; set; }
+
+        public Skill Skill { get; set; }
+    }
+}
