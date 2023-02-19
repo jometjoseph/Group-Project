afa275cdb85b3b6dace56676669dcde4d4f0d92b
jometjoseph <120706019+jometjoseph@users.noreply.github.com>
2023-02-19 11:28:21 +0530

    Add files via upload

---
+++ server/SkillMatrix.Service/Dto/ProfileViewDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillMatrix.Service.Dto
+{
+    public class ProfileViewDto
+    {
+<<<<<<< HEAD
+=======
+        public string Id { get; set; }
+
+>>>>>>> 8e5fdc9 (Server files)
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+    }
+<<<<<<< HEAD
+}
+=======
+
+}
+>>>>>>> 8e5fdc9 (Server files)
